4f6cad585b00ee6cf25794cab5bfd589966daac2
HD111995 <shomawmisho1234@gmail.com>
2021-10-07 21:16:17 +0200

    has been fitched

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+const dotenv = require('dotenv')
+require('dotenv').config()
+const result = dotenv.config()
+const axios = require('axios')
 port = process.env.PORT || 3000
-//const cors = require(cors)
-//app.use(cors())
+var cors = require('cors')
+app.use(cors())
 
-
-app.get('/',(req,res)=>{
-    res.render('pages/index.ejs')
-})
 
 app.listen(port,()=>{
     console.log('i am your listening Server Hi')
+})
+
+app.get('/',(req,res)=>{
+    res.redirect('/pages/1')
+})
+
+app.get('/pages/:page',(req,res)=>{
+    axios.get(`
+    https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=${Number(req.params.page)}`)
+    .then(function(response){
+        
+        res.render('pages/index.ejs',{pages:req.params.page,movies:response.data.results})
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+    
 })
